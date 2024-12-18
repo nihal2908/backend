6ddf55786c601ac530c79cef2f0d02570ff2112d
Nihal Yadav <nihalyadav8524@gmail.com>
2024-12-18 18:34:18 +0530

    only one get api

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hello, world! Your Go API is working."))
+}
+
+func main() {
+	http.HandleFunc("/", helloHandler)
+
+	fmt.Println("Server is running on http://localhost:4000")
+	err := http.ListenAndServe(":4000", nil)
+	if err != nil {
+		fmt.Printf("Error starting server: %s\n", err)
+	}
+}
